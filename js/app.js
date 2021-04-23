1b4d40926503c8df4f54d5b4c2647d8f2bac4eea
Rallanvila <r.allanvila@gmail.com>
2021-04-23 11:59:07 -0400

    completed js slider-style landing page

--- js/app.js
+++ js/app.js
@@ -4,8 +4,41 @@ let init = () =>{
     circles = document.getElementsByClassName('jumbo-slider__circle'),
     links = document.getElementsByClassName('jumbo-slider__link'), 
     current = 1, 
-    time = 6000;
-
+    time = 12000;
+    //add animation class to slide container
     slides[0].classList.add('jumbo-slider__slide--active');
+    links[current-1].classList.add('jumbo-slider__link--active');
+    circles[current-1].classList.add('jumbo-slider__circle--filled')
+
+    //update elipsis and links
+    let updateNav = (current) =>{
+        // console.log(`current: ${current}`)
+        for(let i = 0; i < slides.length; i++){
+            links[i].classList.remove('jumbo-slider__link--active');
+            circles[i].classList.remove('jumbo-slider__circle--filled');
+        }
+        links[current-1].classList.add('jumbo-slider__link--active')
+        circles[current-1].classList.add('jumbo-slider__circle--filled')
+    }
+    let startSliding = () =>{
+        setInterval(() =>{
+            //remove active from first and add it to second slide.
+            slides[1].classList.add('jumbo-slider__slide--active')
+            slides[0].classList.remove('jumbo-slider__slide--active')
+            // clone the first slide and place it last spot and remove after cloned.
+            container.appendChild(slides[0].cloneNode([true]))
+            container.removeChild(slides[0])
+        //   console.log(`slides: $(slides.length)`)
+            if(current < slides.length){
+                current++
+                updateNav(current)
+            } else {
+                current = 1
+                updateNav(current)
+            }
+
+        }, time)
+    }
+    startSliding();
 }
 init()
